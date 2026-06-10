8d2428689dae1a428b1c10e6ae735f60f623ad86
Emmanuel Château-Dutier <emchateau@laposte.net>
2026-06-10 18:53:21 -0400

    corr render

---
+++ src/routes/cours/[slug]/+page.server.ts
@@ -0,0 +1,11 @@
+import type { EntryGenerator } from './$types';
+
+import { loadPublishedCours } from '$lib/utilities/loadPublishedCours';
+
+export const prerender = true;
+
+export const entries: EntryGenerator = async () => {
+	const allPosts = await loadPublishedCours();
+
+	return allPosts.map((post) => ({ slug: post.slug }));
+};
